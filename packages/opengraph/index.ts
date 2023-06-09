51615ed6a9b7c2d7af5fd6a313058ab2167e0eff
SettingDust <settingdust@gmail.com>
2023-06-09 16:43:48 +0800

    feat(opengraph): add dates and author

--- packages/opengraph/index.ts
+++ packages/opengraph/index.ts
@@ -7,32 +7,34 @@ import { map } from 'iterable-operator'
  */
 export default {
   title: function* (input: string) {
-    yield parseHtml(input)
-      .querySelector('meta[property="og:title"]')
-      ?.getAttribute('content')
+    yield parseHtml(input).querySelector('meta[property="og:title"]')?.getAttribute('content')
   },
   url: function* (input: string) {
-    yield parseHtml(input)
-      .querySelector('meta[property="og:url"]')
-      ?.getAttribute('content')
+    yield parseHtml(input).querySelector('meta[property="og:url"]')?.getAttribute('content')
   },
   description: function* (input: string) {
-    yield parseHtml(input)
-      .querySelector('meta[property="og:description"]')
-      ?.getAttribute('content')
+    yield parseHtml(input).querySelector('meta[property="og:description"]')?.getAttribute('content')
   },
   image: (input: string) =>
-    map(
-      [
-        'meta[property="og:image:secure_url"]',
-        'meta[property="og:image:url"]',
-        'meta[property="og:image"]'
-      ],
-      (it) => parseHtml(input).querySelector(it)?.getAttribute('content')
+    map(['meta[property="og:image:secure_url"]', 'meta[property="og:image:url"]', 'meta[property="og:image"]'], (it) =>
+      parseHtml(input).querySelector(it)?.getAttribute('content')
     ),
   language: function* (input: string) {
-    yield parseHtml(input)
-      .querySelector('meta[property="og:locale"]')
-      ?.getAttribute('content')
+    yield parseHtml(input).querySelector('meta[property="og:locale"]')?.getAttribute('content')
+  },
+  author: {
+    url: function* (input: string) {
+      // https://stackoverflow.com/a/29831974
+      yield parseHtml(input).querySelector('meta[property="article:author"]')?.getAttribute('content')
+    },
+  },
+  date: {
+    published: function* (input: string) {
+      yield parseHtml(input).querySelector('meta[property="article:published_time"]')?.getAttribute('content')
+      yield parseHtml(input).querySelector('meta[property*="release_date" i]')?.getAttribute('content')
+    },
+    modified: function* (input: string) {
+      yield parseHtml(input).querySelector('meta[property="article:modified_time"]')?.getAttribute('content')
+    }
   }
 } satisfies Extractors
