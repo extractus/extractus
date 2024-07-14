c7e9a47fbc007c2607ccf31ff9a5b642428c5795
SettingDust <settingdust@gmail.com>
2024-07-15 00:21:11 +0800

    test: fix the test data path for deno

--- examples/deno/index.ts
+++ examples/deno/index.ts
@@ -1,17 +1,17 @@
-import { extract } from "https://esm.sh/@extractus/extractus"
-import {dirname, resolve} from "https://deno.land/std@0.219.0/path/mod.ts";
-import {assertEquals} from "https://deno.land/std@0.218.0/assert/assert_equals.ts";
-import readTextFile = Deno.readTextFile;
+import { extract } from 'https://esm.sh/@extractus/extractus@0.5.0'
+import { relative } from 'https://deno.land/std@0.219.0/path/mod.ts'
+import { assertEquals } from 'https://deno.land/std@0.218.0/assert/assert_equals.ts'
+import readTextFile = Deno.readTextFile
 
 Deno.test('extract', async () => {
-    const html = await readTextFile(resolve(dirname(import.meta.url), 'test/data/meta-test.html'))
-    const result = await extract(html)
-    assertEquals(result, {
-        author: {
-            name: 'jsonld',
-            url: 'https://jsonld.com'
-        },
-        title: 'jsonld',
-        url: 'https://og.com'
-    })
+  const html = await readTextFile(relative(import.meta.dirname!, '../../test/data/meta-test.html'))
+  const result = await extract(html)
+  assertEquals(result, {
+    author: {
+      name: 'jsonld',
+      url: 'https://jsonld.com'
+    },
+    title: 'jsonld',
+    url: 'https://og.com'
+  })
 })
