76a51ff76c2e4e717957f4a3592f69827f63c91b
SettingDust <settingdust@gmail.com>
2024-07-15 00:03:45 +0800

    ci: build the project before release

--- example/deno/index.ts
+++ example/deno/index.ts
@@ -1,10 +1,10 @@
-import { extract } from 'https://esm.sh/@extractus/extractus'
+import { extract } from "https://esm.sh/@extractus/extractus@0.4.1"
 import {dirname, resolve} from "https://deno.land/std@0.219.0/path/mod.ts";
 import {assertEquals} from "https://deno.land/std@0.218.0/assert/assert_equals.ts";
 import readTextFile = Deno.readTextFile;
 
 Deno.test('extract', async () => {
-    const html = await readTextFile(resolve(dirname(import.meta.url), 'test/meta-test.html'))
+    const html = await readTextFile(resolve(dirname(import.meta.url), 'test/data/meta-test.html'))
     const result = await extract(html)
     assertEquals(result, {
         author: {
